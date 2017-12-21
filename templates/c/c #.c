0a77e82c41ebc48c005a13ecd7f082077957775c
Shea690901 <ginny690901@hotmail.de>
2017-12-21 22:02:03 +0100

    <feat>(template): template for c/c-header
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>

---
+++ templates/c/c #.c
@@ -0,0 +1,8 @@
+/**
+ * @file #.c
+ * @Brief    
+ * @author René Müller
+ * @version 0.0.0
+ * @date 2017-12-21
+ */
+
---
+++ templates/c/c #.h
@@ -0,0 +1,11 @@
+/**
+ * @file FILE.h
+ * @Brief    
+ * @author René Müller
+ * @version 0.0.0
+ * @date 2017-12-21
+ */
+
+#ifndef __FILE_H__
+#define __FILE_H__
+#endif  /* __FILE_H__ */
